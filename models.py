1027af5b540c5c3a5d0f8989d71239679bf96ef0
Hiram <hifarrer@gmail.com>
2026-04-19 23:31:48 -0500

    basic1

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import secrets
@@ -9,7 +8,7 @@ import uuid
 
 db = SQLAlchemy()
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
